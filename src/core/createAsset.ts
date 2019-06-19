9ed458715aea16f13c26befca8e1352c8e7c7431
saber2pr <saber2pr@gmail.com>
2019-06-19 13:03:51 +0000

    fix(createAsset.ts): mapping correct.

--- src/core/createAsset.ts
+++ src/core/createAsset.ts
@@ -2,7 +2,7 @@
  * @Author: saber2pr
  * @Date: 2019-06-19 10:19:40
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-06-19 11:17:41
+ * @Last Modified time: 2019-06-19 12:57:20
  */
 import { FS } from '@saber2pr/node'
 import { FileNode } from './ast'
@@ -10,11 +10,14 @@ import { getImports } from './getImports'
 import { dirname, join, extname } from 'path'
 import { resolve_node } from './resolve-node'
 import { compose } from './compose'
+import { async_reduce } from './async-reduce'
 
 export async function createAsset(path: string): Promise<FileNode> {
   const buffer = await FS.readFile(path)
 
-  const absolutify = (dep: string) => join(dirname(path), dep)
+  const absolutify = (dep: string) =>
+    dep.startsWith('.') ? join(dirname(path), dep) : dep
+
   const setExt = (s: string) => s + extname(path)
   const verifyName = compose(
     absolutify,
@@ -23,17 +26,27 @@ export async function createAsset(path: string): Promise<FileNode> {
 
   const text = buffer.toString()
 
-  const deps = getImports(text)
+  const imports = getImports(text)
 
-  return Promise.all(deps.map(dep => resolveDep(dep))).then(deps => ({
+  const deps = await Promise.all(imports.map(dep => resolveDep(dep))).then(
+    deps => deps.map(verifyName)
+  )
+
+  const mapping = await async_reduce(
+    imports,
+    async (receiver, dep) => ({
+      ...receiver,
+      [dep]: verifyName(await resolveDep(dep))
+    }),
+    {}
+  )
+
+  return {
     path,
     text,
-    deps: deps.map(verifyName),
-    mapping: deps.reduce(
-      (receiver, dep) => ({ ...receiver, [dep]: verifyName(dep) }),
-      {}
-    )
-  }))
+    deps,
+    mapping
+  }
 }
 
 const resolveDep = (s: string) => (!s.startsWith('.') ? resolve_node(s) : s)
